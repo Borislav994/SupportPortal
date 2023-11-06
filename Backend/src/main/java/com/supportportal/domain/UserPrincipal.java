b16b4aed55f25f089af6c76294dbe866eec27412
Borislav994 <borislavspasic2609@gmail.com>
2023-11-06 13:11:59 +0100

    Adding Backend part of application

---
+++ Backend/src/main/java/com/supportportal/domain/UserPrincipal.java
@@ -0,0 +1,54 @@
+package com.supportportal.domain;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import static java.util.Arrays.stream;
+
+public class UserPrincipal implements UserDetails {
+
+    private UserEntity userEntity;
+
+    public UserPrincipal(UserEntity userEntity) {
+        this.userEntity = userEntity;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return stream(this.userEntity.getAuthorities()).map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return this.userEntity.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.userEntity.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return this.userEntity.isNotLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.userEntity.isActive();
+    }
+}
