8ecafe9a6335211f9f6bbf3ddb7c8671b31c137a
Borislav994 <borislavspasic2609@gmail.com>
2023-11-07 15:19:03 +0100

    Added exception handler for multiple scene

--- Backend/src/main/java/com/supportportal/resource/UserResource.java
+++ Backend/src/main/java/com/supportportal/resource/UserResource.java
@@ -1,16 +1,19 @@
 package com.supportportal.resource;
 
-import com.supportportal.domain.UserEntity;
+import com.supportportal.exception.ExceptionHandling;
+import com.supportportal.exception.domain.EmailExistException;
+import com.supportportal.exception.domain.UserNotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/user")
-public class UserResource {
+public class UserResource extends ExceptionHandling {
 
     @GetMapping("/home")
-    public String showUser(){
-        return "application work";
+    public String showUser() throws UserNotFoundException {
+        //return "application work";
+        throw new UserNotFoundException("This user not found");
     }
 }
