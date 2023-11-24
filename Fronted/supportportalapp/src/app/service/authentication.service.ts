82f85e1706c7365049cb7c00e5564b09b98be0ba
Borislav994 <borislavspasic2609@gmail.com>
2023-11-24 15:47:31 +0100

    Added user.service and authentication.service

---
+++ Fronted/supportportalapp/src/app/service/authentication.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+  
+  private host = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  public login(user: User): Observable<HttpResponse<any> | HttpErrorResponse> {
+    return this.http.post<HttpResponse<any> | HttpErrorResponse>(`${this.host}/user/login`, user, {observe: 'response'});
+  }
+}
