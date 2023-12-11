d32d6fcbdb3393ed077c1b5af97f633410e9b6c2
Borislav994 <borislavspasic2609@gmail.com>
2023-12-11 13:12:04 +0100

    Finished register component and all logic about register.

--- Fronted/supportportalapp/src/app/component/register/register.component.ts
+++ Fronted/supportportalapp/src/app/component/register/register.component.ts
@@ -1,15 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { NotificationType } from 'src/app/enum/notification-type.enum';
+import { User } from 'src/app/model/user';
+import { AuthenticationService } from 'src/app/service/authentication.service';
+import { NotificationService } from 'src/app/service/notification.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+ 
+  public showLoading: boolean;
+  private subscriptions: Subscription[] = [];
+
+  constructor(private router: Router, private authService: AuthenticationService, private notificationService: NotificationService) { }
 
   ngOnInit(): void {
+    if(this.authService.isLoggedIn()){
+      this.router.navigateByUrl('/user/management');
+    }
   }
 
+  public onRegister(user: User): void {
+    this.showLoading = true;
+    console.log(user);
+  
+    this.subscriptions.push(
+      this.authService.register(user).subscribe(
+        (response: User) => {
+          this.showLoading = false;
+          this.sendNotification(NotificationType.SUCCESS, `A new account was created for ${response.firstName}. Please chack your email for password to log in.`);
+
+        },
+        (errorResponse: HttpErrorResponse) => {
+          console.log(errorResponse);
+          this.sendNotification(NotificationType.ERROR, errorResponse.error.message);
+          this.showLoading = false;
+        }
+      )
+    );
+  }
+
+  private sendNotification(notificationType: NotificationType, message: string): void {
+    if(message) {
+      this.notificationService.notify(notificationType, message);
+    } else {
+      this.notificationService.notify(notificationType, 'An error occured. please try again.');
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(sub => sub.unsubscribe());
+  }
 }
